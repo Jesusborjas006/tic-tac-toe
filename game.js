fbc4c3a362c636491edc2457d2dcca7683cc4ba3
Jesus Borjas <jesusbor006@gmail.com>
2022-11-11 10:36:53 -0600

    Add new way to check for click event

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 class Game {
   constructor() {
     this.players = [player1, player2]
-    this.playerTurn = player1.token // "❎"
   } 
 }
 
@@ -11,115 +10,64 @@ var player2 = new Player(2, '🅾️', 0);
 var game = new Game({ player1, player2 });
 console.log(game)
 
-var heading;
-
 var buttons = document.querySelectorAll('.button');
-console.log(buttons)
+// console.log(buttons)
 var btnClicked = []
-// console.log(btnClicked)
 
-function resetBoard() {
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].innerText = ''
-  }
-}
-resetBoard()
+// var grid = document.querySelector('.grid-container')
+// grid.addEventListener('click', myFunction)
 
-// function addToken(event) {
-//   for (var i = 0; i < buttons.length; i++) {
-//     buttons[i].innerText = ''
-//     buttons[i].addEventListener('click', function() {
-//       alert(event.target.id)
-//       // if(buttons[0].id === 'button1') {
-//       //   console.log('Btn1 clicked')
-//       // } else if (buttons[1].id === 'button2') {
-//       //   console.log('Btn2 clicked')
-//       // }
-//     })
+// function myFunction(event) {
+//   if(event.target.id === 'button1' && !btnClicked.includes()) {
+//     console.log(buttons[0])
+//     btnClicked.push(event.target.id);
+//     console.log(btnClicked)
+//     console.log(event.target.id)
+//     // fix players turn with a function
+//   } else if(event.target.id === 'button2' && buttons[1].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button3' && buttons[2].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button4' && buttons[3].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button5' && buttons[4].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button6' && buttons[5].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button7' && buttons[6].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button8' && buttons[7].innerText === '') {
+//     console.log(event.target.id)
+//   } else if(event.target.id === 'button9' && buttons[8].innerText === '') {
+//     console.log(event.target.id)
 //   }
 // }
-// addToken()
 
-var grid = document.querySelector('.grid-container')
-grid.addEventListener('click', myFunction)
-
-function myFunction(event) {
-  if(event.target.id === 'button1' && buttons[0].innerText === '') {
-    console.log(event.target.id)
-    buttons[0].innerText = '❎'
-  } else if(event.target.id === 'button2' && buttons[1].innerText === '') {
-    console.log(event.target.id)
-    buttons[1].innerText = '❎'
-  } else if(event.target.id === 'button3' && buttons[2].innerText === '') {
-    console.log(event.target.id)
-    buttons[2].innerText = '❎'
-  } else if(event.target.id === 'button4' && buttons[3].innerText === '') {
-    console.log(event.target.id)
-    buttons[3].innerText = '❎'
-  } else if(event.target.id === 'button5' && buttons[4].innerText === '') {
-    console.log(event.target.id)
-    buttons[4].innerText = '❎'
-  } else if(event.target.id === 'button6' && buttons[5].innerText === '') {
-    console.log(event.target.id)
-    buttons[5].innerText = '❎'
-  } else if(event.target.id === 'button7' && buttons[6].innerText === '') {
-    console.log(event.target.id)
-    buttons[6].innerText = '❎'
-  } else if(event.target.id === 'button8' && buttons[7].innerText === '') {
-    console.log(event.target.id)
-    buttons[7].innerText = '❎'
-  } else if(event.target.id === 'button9' && buttons[8].innerText === '') {
-    console.log(event.target.id)
-    buttons[8].innerText = '❎'
-  }
+// Pushes the data into the btnClicked array if not there
+for (var i = 0; i < buttons.length; i++) {
+  // console.log(buttons[i])
+  buttons[i].addEventListener('click', function(event) {
+    console.log(event.target)
+    if(!btnClicked.includes(event.target.id)) {
+      btnClicked.push(event.target.id)
+      console.log(btnClicked)
+    } else {
+      console.log("Alredy In the array!!!")
+    }
+  })
 }
 
+console.log(btnClicked)
 
-
-
-
-
-
-
-// Switches player turn
-function switchTurn() {
-  if (game.playerTurn === player1.token) {
-    playerTurn = player2.token
-    console.log(heading = `It's ${player2.token}'s turn`)
-    heading.innerText = `It's ${player2.token}'s turn`
-  } else {
-    playerTurn = player1.token
-    console.log(heading = `It's ${player1.token}'s turn`)
-    heading = `It's ${player1.token}'s turn`
-  }
-}
-// switchTurn()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function storeBtn() {
+//   if(!btnClicked.includes()) {
+//     btnClicked.push(buttons[i])
+//     console.log(btnClicked)
+//   } else {
+//     return
+//   }
+// }
+// storeBtn()
 // Pseudocode 
 
 //  1. Create a global variable to track the wins for a player
@@ -128,6 +76,9 @@ function switchTurn() {
 //  4. If no player wins say "It's a draw" else display winner
 //  5. Add 1 point to counter of player if won
 //  6. Change players turn when game is over 
+
+
+// Create all global varibales ()
 
 
 
