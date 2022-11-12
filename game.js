02168f7ec31d184c3afb83f98a7adcb5385c04a5
Jesus Borjas <jesusbor006@gmail.com>
2022-11-12 14:13:08 -0600

    Add: Switch player function

--- game.js
+++ game.js
@@ -7,67 +7,67 @@ class Game {
 var player1 = new Player(1, '❎', 0);
 var player2 = new Player(2, '🅾️', 0);
 
-var game = new Game({ player1, player2 });
-console.log(game)
+var player1Token = player1.token;
+var player2Token = player2.token;
+var currentPlayer = player1Token;
 
+var game = new Game({ player1, player2 });
 var buttons = document.querySelectorAll('.button');
-// console.log(buttons)
 var btnClicked = []
 
-// var grid = document.querySelector('.grid-container')
-// grid.addEventListener('click', myFunction)
-
-// function myFunction(event) {
-//   if(event.target.id === 'button1' && !btnClicked.includes()) {
-//     console.log(buttons[0])
-//     btnClicked.push(event.target.id);
-//     console.log(btnClicked)
-//     console.log(event.target.id)
-//     // fix players turn with a function
-//   } else if(event.target.id === 'button2' && buttons[1].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button3' && buttons[2].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button4' && buttons[3].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button5' && buttons[4].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button6' && buttons[5].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button7' && buttons[6].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button8' && buttons[7].innerText === '') {
-//     console.log(event.target.id)
-//   } else if(event.target.id === 'button9' && buttons[8].innerText === '') {
-//     console.log(event.target.id)
-//   }
-// }
-
-// Pushes the data into the btnClicked array if not there
+// Pushes the data into the btnClicked array if not there and add token
 for (var i = 0; i < buttons.length; i++) {
-  // console.log(buttons[i])
   buttons[i].addEventListener('click', function(event) {
     console.log(event.target)
     if(!btnClicked.includes(event.target.id)) {
       btnClicked.push(event.target.id)
+      addToken(game)
+      switchPlayer()
       console.log(btnClicked)
+      // return btnClicked
     } else {
-      console.log("Alredy In the array!!!")
+      console.log("Already in array and token was already placed")
     }
   })
 }
 
 console.log(btnClicked)
 
-// function storeBtn() {
-//   if(!btnClicked.includes()) {
-//     btnClicked.push(buttons[i])
-//     console.log(btnClicked)
-//   } else {
-//     return
-//   }
+
+function addToken(game) {
+  if(btnClicked.includes('button1')) {
+    console.log('Token Added')
+    buttons[0].innerText = game.players[0].token
+  } else {
+    console.log('Not Yet Added!')
+  }
+}
+
+function switchPlayer() {
+  if(currentPlayer === player1Token) {
+    currentPlayer = player2Token 
+    console.log(currentPlayer)
+    console.log("Player has been switched")
+  } else {
+    currentPlayer = player1Token 
+    console.log(currentPlayer)
+    console.log("Player has been switched")
+  }
+  
+}
+
+
+
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', function(event) {
+//     if(!btnClicked.includes(event.target.id)) {
+  
+//     } else {
+//       console.log("Token already displayed")
+//     }
+//   })
 // }
-// storeBtn()
+
 // Pseudocode 
 
 //  1. Create a global variable to track the wins for a player
