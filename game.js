e3762ae49fa69e79584d944ca1bca1f71ea88d5d
Jesus Borjas <jesusbor006@gmail.com>
2022-11-14 14:48:49 -0600

    Add: Winner when first row has same tokens

--- game.js
+++ game.js
@@ -15,6 +15,29 @@ var game = new Game({ player1, player2 });
 var buttons = document.querySelectorAll('.button');
 var btnClicked = []
 
+var winCount1 = 0;
+var winCount2 = 0;
+var winner;
+
+var combo1 = [buttons[0], buttons[1], buttons[2]];
+console.log(combo1)
+
+// How to know who the winner is?
+
+// Look for all the possible combinations for the board
+
+//  1,2,3
+//  4,5,6
+//  7,8,9
+//  1,4,7
+//  2,5,8
+//  3,6,9
+//  1,5,9
+//  3,5,7
+
+// If players token equals to all three numbers they win
+
+
 // Pushes the data into the btnClicked array if not there and add token
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function(event) {
@@ -24,53 +47,37 @@ for (var i = 0; i < buttons.length; i++) {
       addToken()
       switchPlayer()
       console.log(btnClicked)
+      declareWinner()
     } else {
       console.log("Already in array and token was already placed")
     }
   })
 }
 
-console.log(btnClicked)
-
-
 // Adds token to board if not yet added
 function addToken() {
   if(btnClicked.includes('button1') && buttons[0].innerText === '') {
-    console.log('Token Added')
     buttons[0].innerText = currentPlayer
   } else if (btnClicked.includes('button2') && buttons[1].innerText === '') {
-    console.log('Token Added')
     buttons[1].innerText = currentPlayer
   } else if (btnClicked.includes('button3') && buttons[2].innerText === '') {
-    console.log('Token Added')
     buttons[2].innerText = currentPlayer
   } else if (btnClicked.includes('button4') && buttons[3].innerText === '') {
-    console.log('Token Added')
     buttons[3].innerText = currentPlayer
   } else if (btnClicked.includes('button5') && buttons[4].innerText === '') {
-    console.log('Token Added')
     buttons[4].innerText = currentPlayer
   } else if (btnClicked.includes('button6') && buttons[5].innerText === '') {
-    console.log('Token Added')
     buttons[5].innerText = currentPlayer
   } else if (btnClicked.includes('button7') && buttons[6].innerText === '') {
-    console.log('Token Added')
     buttons[6].innerText = currentPlayer
   } else if (btnClicked.includes('button8') && buttons[7].innerText === '') {
-    console.log('Token Added')
     buttons[7].innerText = currentPlayer
   } else if (btnClicked.includes('button9') && buttons[8].innerText === '') {
-    console.log('Token Added')
     buttons[8].innerText = currentPlayer
-  } 
-  
-  
-  
-  else {
+  } else {
     console.log('Not Yet Added!')
   }
 }
-
 // Swaps player turn when choosing a btn
 function switchPlayer() {
   if(currentPlayer === player1Token) {
@@ -83,6 +90,19 @@ function switchPlayer() {
     console.log("Player has been switched")
   }
 }
+
+function declareWinner() {
+    if(combo1[0].innerText === '❎' && combo1[1].innerText === '❎' && combo1[2].innerText === '❎') {
+      console.log("We have a winner!")
+    } else {
+      console.log("No winner")
+  }
+}
+
+
+
+
+
 
 
 // Pseudocode 
